5ad19eaeb1295ef330e6e19727e3a39aafc916ba
Ivo Verberk <iverberk@xebia.com>
2021-11-05 09:59:54 +0100

    Update Helm chart versions

---
+++ component/component.go
@@ -0,0 +1,19 @@
+package cicd
+
+import "github.com/opsteady/opsteady/cli/component"
+
+// KubernetesAWSLoadbalancing is a component for the AWS load balancer controller.
+type KubernetesAWSLoadbalancing struct {
+	component.DefaultComponent
+}
+
+// Initialize creates a new KubernetesAWSLoadbalancing component
+func (k *KubernetesAWSLoadbalancing) Initialize(defaultComponent component.DefaultComponent) {
+	k.DefaultComponent = defaultComponent
+	k.DefaultComponent.RequiresComponents("foundation-aws", "kubernetes-aws-cluster")
+	k.DefaultComponent.SetVaultInfoToComponentConfig()
+	k.DefaultComponent.UseHelm(component.NewHelmChart(
+		"aws-load-balancer-controller",
+		"1.3.2", // renovate: datasource=helm registryUrl=https://aws.github.io/eks-charts depName=aws-load-balancer-controller versioning=semver
+	))
+}
